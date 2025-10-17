7bbc253324d516fb93c790f92e14e4eeaec32da8
Al-Amin <37735466+thealamindev@users.noreply.github.com>
2025-10-17 12:59:51 +0600

    Add Header component and integrate it into RootLayout

---
+++ components/shared/header/index.tsx
@@ -0,0 +1,15 @@
+
+import { ShoppingCart, UserIcon } from "lucide-react"
+import Image from "next/image"
+import Link from "next/link"
+
+
+const Header = () => {
+  return (
+    <>
+    Header
+    </>
+  )
+}
+
+export default Header
